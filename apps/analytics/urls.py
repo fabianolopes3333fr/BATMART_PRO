9804e02d4f1e229cb37f92d4260f2688afb38c0c
fabianolopes3333fr <meuiptvbr@gmail.com>
2025-01-28 20:33:28 +0100

    Implementação de templates e views para métricas no módulo de analytics
    - Criado template metric_detail.html
    - Criado template metric_form.html para criação e edição de métricas
    - Criado template metric_confirm_delete.html para confirmação de exclusão
    - Atualizadas URLs para as novas views
    - Adicionadas traduções em francês para os novos textos
    - Implementadas views MetricCreateView, MetricUpdateView e MetricDeleteView

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReportExecutionCreateView, ReportExecutionDeleteView, ReportExecutionDetailView, ReportExecutionListView, ReportExecutionUpdateView, ReportListView, ReportCreateView, ReportDetailView, ReportUpdateView, ReportDeleteView
+from .views import DashboardCreateView, DashboardDeleteView, DashboardDetailView, DashboardListView, DashboardUpdateView, MetricCreateView, MetricDeleteView, MetricDetailView, MetricListView, MetricUpdateView, ReportExecutionCreateView, ReportExecutionDeleteView, ReportExecutionDetailView, ReportExecutionListView, ReportExecutionUpdateView, ReportListView, ReportCreateView, ReportDetailView, ReportUpdateView, ReportDeleteView
 
 app_name = 'analytics'
 
@@ -15,4 +15,16 @@ urlpatterns = [
     path('report-executions/<int:pk>/', ReportExecutionDetailView.as_view(), name='report_execution_detail'),
     path('report-executions/<int:pk>/update/', ReportExecutionUpdateView.as_view(), name='report_execution_update'),
     path('report-executions/<int:pk>/delete/', ReportExecutionDeleteView.as_view(), name='report_execution_delete'),
+    
+    path('dashboards/', DashboardListView.as_view(), name='dashboard_list'),
+    path('dashboards/create/', DashboardCreateView.as_view(), name='dashboard_create'),
+    path('dashboards/<int:pk>/', DashboardDetailView.as_view(), name='dashboard_detail'),
+    path('dashboards/<int:pk>/update/', DashboardUpdateView.as_view(), name='dashboard_update'),
+    path('dashboards/<int:pk>/delete/', DashboardDeleteView.as_view(), name='dashboard_delete'),
+    
+    path('metrics/', MetricListView.as_view(), name='metric_list'),
+    path('metrics/create/', MetricCreateView.as_view(), name='metric_create'),
+    path('metrics/<int:pk>/', MetricDetailView.as_view(), name='metric_detail'),
+    path('metrics/<int:pk>/update/', MetricUpdateView.as_view(), name='metric_update'),
+    path('metrics/<int:pk>/delete/', MetricDeleteView.as_view(), name='metric_delete'),
 ]
